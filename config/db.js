7cbf1e247121b8a1ca3d9074fcf1f88053f8873f
saim-x <x_aci@hotmail.com>
2024-02-01 12:31:36 +0500

    commit

--- config/db.js
+++ config/db.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 
 const connection = mongoose.createConnection('mongodb://localhost:27017').on('open', () => {
     console.log('MongoDB: Connected');
+}).on('error', (err) => {
+    console.log('MongoDB: Error', err);
 });
 
 
